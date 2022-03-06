4e8139a014eacdbee76aaa48087ce36248a90228
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2022-03-06 19:53:04 -0300

    altera biblioteca de extracao

--- etl/run.py
+++ etl/run.py
@@ -1,12 +1,20 @@
-import wget
+from urllib.request import urlretrieve
 import py7zr
 import os
 
-basepath = "opt/ml/processing/output"
+basepath = "/opt/ml/processing/output"
 dlpath = f"{basepath}/rais"
 
+names = [
+     "RAIS_VINC_PUB_CENTRO_OESTE.7z",
+     "RAIS_VINC_PUB_NORDESTE.7z",
+     "RAIS_VINC_PUB_NORTE.7z",
+     "RAIS_VINC_PUB_SUL.7z",
+     "RAIS_VINC_PUB_MG_ES_RJ.7z",
+     "RAIS_VINC_PUB_SP.7z",
+]
 urls = [
-    "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_CENTRO_OESTE.7z",
+    "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_CENTRO_OESTE.7z"
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_NORDESTE.7z",
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_NORTE.7z",
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_SUL.7z",
@@ -14,8 +22,10 @@ urls = [
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_SP.7z",
 ]
 
-def obter_dados(url):
-    filename = wget.download(url, out=dlpath)
+def obter_dados(url, name):
+    filename = dlpath + '/' + name
+    urlretrieve(url, filename=filename)
+    print(filename)
     archive = py7zr.SevenZipFile(filename)
     archive.extractall(path=dlpath)
     archive.close()
@@ -25,9 +35,9 @@ def obter_dados(url):
 if __name__ == "__main__":
     os.makedirs(dlpath, exist_ok=True)
 
-    for url in urls:
-        print(f"Extracting from {url}")
-        res = obter_dados(url)
+    for i in range(len(urls)):
+        print(f"Extracting from {urls[i]}")
+        res = obter_dados(urls[i], names[i])
         print(res)
     
     print("Done!")
