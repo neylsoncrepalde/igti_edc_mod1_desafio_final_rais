960b90cefd238083496e1ca67bc60b700e1071eb
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2022-03-06 18:02:13 -0300

    bug/tira acl de aws_s3_bucket

--- infrastructure/aws/storage.tf
+++ infrastructure/aws/storage.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "buckets" {
   count  = length(var.bucket_names)
   bucket = "${local.prefix}-${var.bucket_names[count.index]}-${var.account}"
-  acl    = "private"
 
   tags = local.common_tags
 
