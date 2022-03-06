501d4dfc6e9d62789e6a3a98123ffa3f4deb08b1
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2022-03-06 20:58:25 -0300

    small correction on extraction run.py

--- etl/run.py
+++ etl/run.py
@@ -14,7 +14,7 @@ names = [
      "RAIS_VINC_PUB_SP.7z",
 ]
 urls = [
-    "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_CENTRO_OESTE.7z"
+    "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_CENTRO_OESTE.7z",
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_NORDESTE.7z",
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_NORTE.7z",
     "ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/2020/RAIS_VINC_PUB_SUL.7z",
