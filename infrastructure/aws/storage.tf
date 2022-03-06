fb3d0651eb5b444821608c7aae80aaba89d06701
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2022-03-06 18:04:23 -0300

    update to new s3 terraform configuration

--- infrastructure/aws/storage.tf
+++ infrastructure/aws/storage.tf
@@ -3,14 +3,6 @@ resource "aws_s3_bucket" "buckets" {
   bucket = "${local.prefix}-${var.bucket_names[count.index]}-${var.account}"
 
   tags = local.common_tags
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 
